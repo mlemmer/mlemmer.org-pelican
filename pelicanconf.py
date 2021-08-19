5576c4abd89ce85178bc4761dec60446c7b4716c
Christine Lemmer-Webber <cwebber@dustycloud.org>
2021-08-19 09:45:58 -0400

    Make the pages appear under better URIs

--- pelicanconf.py
+++ pelicanconf.py
@@ -34,6 +34,8 @@ DEFAULT_PAGINATION = 10
 ARTICLE_URL = "blog/{slug}/"
 ARTICLE_SAVE_AS = "blog/{slug}/index.html"
 
+PAGE_URL = "{slug}/"
+PAGE_SAVE_AS = "{slug}/index.html"
 
 STATIC_PATHS  = [
     "images",
